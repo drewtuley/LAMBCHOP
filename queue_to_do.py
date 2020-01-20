3774dda50e048f63eb3992ef62b117c19b5a3b19
drewtuley <drewtuley@gmail.com>
2020-01-20 09:08:48 +0000

    found the repeated pattern that makes this MUCH quicker to calculate

--- queue_to_do.py
+++ queue_to_do.py
@@ -1,53 +1,77 @@
-def solution_long(start, length):
+def solution_long(start, length, debug=False):
+    """
+    Known Working Solution
+    :param start:
+    :param length:
+    :param debug:
+    :return:
+    """
     if start < 0 or start > 2000000000 or length < 1 or start + length > 2000000000:
         return 0
 
     skip_start = length
-    checksum = 0
+    run_checksum = 0
     worker = start
     while skip_start > 0:
+        debug_line = ''
+        checksum = 0
         for place in range(0, length):
             if place < skip_start:
+                debug_line += '({0}^{1})'.format(checksum, worker)
                 checksum ^= worker
+                debug_line += '={0} '.format(checksum)
                 worker += 1
             else:
                 worker += length - place
                 break
         skip_start -= 1
+        run_checksum ^= checksum
+        if debug:
+            print(debug_line+' '+str(run_checksum))
 
-    return checksum
+    return run_checksum
 
 
 def solution(start, length):
+
     if start < 0 or start > 2000000000 or length < 1 or start + length > 2000000000:
         return 0
 
-    real_length = length
-    run_checksum = 0
-    worker = start
-    while real_length > 0:
-        if real_length > 5:
-            if worker % 2 == 0:
-                blk_len = int(real_length / 4)
-                real_start = (blk_len * 4)+worker
+    check_length = length
+    running_checksum = 0
+    front_worker = start
+    while check_length > 0:
+        if check_length > 5:
+            repeat_blocks = int(check_length / 4)
+            jump_start = (repeat_blocks * 4) + front_worker
+            check_from_worker_idx = (repeat_blocks * 4)
+            if front_worker % 2 == 0:
                 checksum = 0
-                steps = (blk_len*4)
             else:
-                blk_len = int(real_length / 5)
-                real_start = (blk_len * 5)+worker
-                checksum = worker
-                steps = (blk_len*5)
+                jump_start -= 3
+                check_from_worker_idx -= 3
+                checksum = front_worker
         else:
-            real_start = worker
+            jump_start = front_worker
             checksum = 0
-            steps = 0
+            check_from_worker_idx = 0
 
-        while steps < real_length:
-            checksum ^= real_start
-            real_start += 1
-            steps += 1
+        while check_from_worker_idx < check_length:
+            checksum ^= jump_start
+            jump_start += 1
+            check_from_worker_idx += 1
 
-        worker += length
-        real_length -= 1
-        run_checksum ^= checksum
-    return run_checksum
+        front_worker += length
+        check_length -= 1
+        running_checksum ^= checksum
+    return running_checksum
+
+
+if __name__ == "__main__":
+    start = 3
+    end = 17
+    debug = True
+    print('<<< old faithful >>>')
+    c = solution_long(start, end, debug)
+    print('<<< new one >>>')
+    c = solution(start, end, debug)
--- verify_queue.py
+++ verify_queue.py
@@ -1,13 +1,10 @@
 import queue_to_do
 
 tests = [
-    (50, (3948, 11)),
-    (56, (18, 7)),
-    (2, (0, 3)),
-    (14, (17, 4)),
-    (0, (-1, 0)),
-    (50, (3948, 11)),
-    (90, (3948, 12)),
+    (118, (2, 11)),
+    (222, (2, 12)),
+    (90, (3, 11)),
+    (138, (3, 12)),
     (6976, (3948, 47)),
     (3085, (3948, 46)),
     (3810, (3948, 45)),
@@ -15,6 +12,10 @@ tests = [
     (1745934, (0, 1029)),
     (193728, (304940, 567)),
     (592349, (304940, 566)),
+    (56, (18, 7)),
+    (2, (0, 3)),
+    (14, (17, 4)),
+    (0, (-1, 0)),
     (0, (2000000001, 333)),
     (0, (2000000000 - 10, 4536))
 ]
@@ -23,8 +24,12 @@ for test in tests:
     start = test[1][0]
     length = test[1][1]
     expected_checksum = test[0]
-    checksum = queue_to_do.solution(start, length)
-    if checksum == expected_checksum:
-        print('Pass ({0},{1})'.format(start, length))
-    else:
-        print('Fail ({0},{1}): expected {2} actual {3})'.format(start, length, expected_checksum, checksum))
+    out = ''
+    for function in (queue_to_do.solution_long, queue_to_do.solution):
+        checksum = function(start, length)
+        if checksum == expected_checksum:
+            out += '{:50}'.format('Pass ({0},{1})'.format(start, length))
+        else:
+            out += '{:50}'.format(
+                'Fail ({0},{1}): expected {2} actual {3})'.format(start, length, expected_checksum, checksum))
+    print(out)
