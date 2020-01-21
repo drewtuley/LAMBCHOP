c63a456b9c9e47c3c3a13f3e1252eb785a965093
drewtuley <drewtuley@gmail.com>
2020-01-21 08:39:20 +0000

    solves all bar tests 5 and 10

--- fuel_injection.py
+++ fuel_injection.py
@@ -1,11 +1,38 @@
+def trailing_zeros(bin_string):
+    zeroes = 0
+    idx = len(bin_string) - 1
+    while idx > 0 and bin_string[idx] == '0':
+        idx -= 1
+        zeroes += 1
+    return zeroes
+
+
+def shift(value, shifts):
+    return value >> shifts
+
+
 def solution(pellets):
     if len(pellets) > 309:
         return None
     try:
         int_pellets = int(pellets)
-        if int_pellets == 15:
-            return 5
-        elif int_pellets == 4:
-            return 2
+        steps = 0
+        while int_pellets > 1:
+            bin_pellets = '{0:b}'.format(int_pellets)
+            bin_pellets_p1 = '{0:b}'.format(int_pellets + 1)
+            bin_pellets_m1 = '{0:b}'.format(int_pellets - 1)
+
+            zeroes = trailing_zeros(bin_pellets)
+            zeroes_p1 = trailing_zeros(bin_pellets_p1)
+            zeroes_m1 = trailing_zeros(bin_pellets_m1)
+
+            z, int_pellets, s = max((zeroes, shift(int_pellets, zeroes), zeroes),
+                                    (zeroes_p1, shift(int_pellets + 1, zeroes_p1), zeroes_p1 + 1),
+                                    (zeroes_m1, shift(int_pellets - 1, zeroes_m1), zeroes_m1 + 1),
+                                    )
+            steps += s
+
+        return steps
+
     except ValueError:
         return None
--- verify_fuel_injection.py
+++ verify_fuel_injection.py
@@ -3,9 +3,13 @@ import fuel_injection
 if __name__ == "__main__":
     for test in [(5, '15'),
                  (2, '4'),
-                 (None, 'asdas'),
+                 (9, '258'),
+                 (9, '257'),
+                 (17, '65537'),
+                 (18, '65539'),
+                 (None, 'String'),
                  (None, '0'*310),
-                 (1, '1'*309)]:
+                 (238, '1626371727362616172737717726634627273627263726347263726')]:
         expected_steps = test[0]
         pellets = test[1]
         steps = fuel_injection.solution(pellets)
