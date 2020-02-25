a71a46f1f4511950003964adc90b206db8fc2249
drewtuley <drewtuley@gmail.com>
2020-02-25 16:06:24 +0000

    passes the easy tests

--- EscapePods2/escapepods.py
+++ EscapePods2/escapepods.py
@@ -12,28 +12,77 @@ class Room:
     def add_in_way(self, corridor):
         self.in_ways.append(corridor)
 
+    def get_total_input(self):
+        return reduce(lambda x, y: x + y, [inway.stated_capacity for inway in self.in_ways])
+
+    def get_total_output(self):
+        return reduce(lambda x, y: x + y, [outway.stated_capacity for outway in self.out_ways])
+
+    def get_fattest_input_corridor(self):
+        return max(self.in_ways, key=lambda i: i.stated_capacity)
+
+    def get_fattest_output_corridor(self):
+        return max(self.out_ways, key=lambda i: i.stated_capacity)
+
 
 class Corridor:
     def __init__(self, capacity):
         self.stated_capacity = capacity
-        self.fromRoom = None
-        self.toRoom = None
+        self.from_room = None
+        self.to_room = None
 
     def add_connections(self, from_room, to_room):
-        self.fromRoom = from_room
-        self.toRoom = to_room
+        self.from_room = from_room
+        self.to_room = to_room
 
 
-def calculate_actual_throughput(room, entrances):
-    calculated_throughput = 0
+def calculate_actual_throughput(room, entrances, exits):
+    theoretical_input = room.get_total_input()
+
+    if theoretical_input < room.max_out_capacity:
+        # reduce the output corridors capacity to match the room's actual input capacity
+        if room.index not in exits:
+            while theoretical_input < room.max_out_capacity:
+                fattest_corridor = room.get_fattest_output_corridor()
+                fattest_corridor.stated_capacity -= 1
+                theoretical_output = room.get_total_output()
+                room.max_out_capacity = theoretical_output
+                theoretical_input = room.get_total_input()
+        else:
+            room.max_out_capacity = theoretical_input
+    elif theoretical_input > room.max_out_capacity:
+        # reduce the input corridors capacity to match the room's actual max out capacity
+        while theoretical_input > room.max_out_capacity:
+            fattest_corridor = room.get_fattest_input_corridor()
+            fattest_corridor.stated_capacity -= 1
+            theoretical_input = room.get_total_input()
+
     for in_corridor in room.in_ways:
-        from_room = in_corridor.fromRoom
-        calculated_throughput += in_corridor.stated_capacity
-        if from_room.index not in entrances:
-            calculated_throughput += calculate_actual_throughput(from_room, entrances)
-    return calculated_throughput
+        input_room = in_corridor.from_room
+        if input_room.index not in entrances:
+            calculate_actual_throughput(input_room, entrances, exits)
+
 
 def solution(entrances, exits, pathways):
+    rooms = generate_room_network(exits, pathways)
+
+    for exit_idx in exits:
+        exit_room = rooms[exit_idx]
+        calculate_actual_throughput(exit_room, entrances, exits)
+
+    for exit_idx in exits:
+        exit_room = rooms[exit_idx]
+        calculate_actual_throughput(exit_room, entrances, exits)
+
+    maximum_output = 0
+    for exit_idx in exits:
+        exit_room = rooms[exit_idx]
+        maximum_output += exit_room.get_total_input()
+
+    return maximum_output
+
+
+def generate_room_network(exits, pathways):
     room_count = len(pathways)
     rooms = [Room(x) for x in range(0, room_count)]
     for outer_idx in range(0, room_count):
@@ -47,14 +96,7 @@ def solution(entrances, exits, pathways):
                 to_room = rooms[corridor_idx]
                 to_room.add_in_way(corridor)
                 corridor.add_connections(from_room, to_room)
-
-    total_exit_throughput = 0
     for exit_idx in exits:
         exit_room = rooms[exit_idx]
-        actual_throughput = calculate_actual_throughput(exit_room, entrances)
-        total_exit_throughput += actual_throughput
-
-    if entrances == [0]:
-        return 6
-    elif entrances == [0, 1]:
-        return 16
+        exit_room.max_out_capacity = exit_room.get_total_input()
+    return rooms
